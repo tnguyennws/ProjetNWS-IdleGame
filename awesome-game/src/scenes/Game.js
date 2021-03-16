e628de06151265545a1f6fd8fb62ead477a78e62
Guenole76 <54894682+Guenole76@users.noreply.github.com>
2021-03-16 18:23:45 +0100

    scene

--- awesome-game/src/scenes/Game.js
+++ awesome-game/src/scenes/Game.js
@@ -49,6 +49,9 @@ export default class Game extends Phaser.Scene
     preload()
     {
         this.load.image('space-ship', 'assets/ship1.png')
+        this.load.image('haland', 'assets/haland.jpg')
+        this.load.image('hero', 'assets/electru.jpg')
+
     }
 
     create()
@@ -58,10 +61,18 @@ export default class Game extends Phaser.Scene
         this.add.image(480, 320, 'background')
         .setScrollFactor(0, 0)
         
-        this.player = this.physics.add.sprite(240, 320, 'space-ship')
+        this.player = this.physics.add.sprite(100, 320, 'space-ship')
         .setScale(0.5)
 
-        this.input.on('space-ship',this.onObjectClicked);
+        this.haland = this.physics.add.sprite(700, 100, 'haland')
+        .setScale(0.5)
+
+        this.hero = this.physics.add.sprite(700, 320, 'hero')
+        .setScale(0.5)
+        game.physics.arcade.enable(this.hero);
+
+        this.cursors = game.input.keyboard.createCursorKeys();
+
  
     }
     onObjectClicked()
@@ -72,17 +83,28 @@ export default class Game extends Phaser.Scene
     update(t, dt)
     {
 
-        if(this.beaconsCollected > 5)
-        {
-            this.scene.start('victory')
-        }
+        let vX = 0;
+        let vY = 0;
 
-        if(this.healthPoint == 0)
+        if (this.cursors.left.isDown)
         {
-            this.scene.start('game-over')
+            vX = -200
         }
-
-     
+        if (this.cursors.right.isDown)
+        {
+            vX = 200
+        }
+        if (this.cursors.down.isDown)
+        {
+            vY = 200
+        }
+        if (this.cursors.up.isDown)
+        {
+            vY = -200
+        }
+        
+        this.hero.setVelocityX(vX);
+        this.hero.setVelocityY(vY);
     }
 
 
