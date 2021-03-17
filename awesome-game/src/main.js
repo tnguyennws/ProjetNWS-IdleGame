51949fbe06f04383f844a63d44cf2e5b58fd10d8
Guenole76 <Gunol>
2021-03-17 10:18:36 +0100

    perso

--- awesome-game/src/main.js
+++ awesome-game/src/main.js
@@ -2,20 +2,19 @@ import Phaser from './lib/phaser.js'
 import Village from './scenes/Village.js'
 
 import MainScreen from './scenes/MainScreen.js'
+import Game from './scenes/Game.js'
 
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 1340,
     height: 640,
-    scene: [MainScreen, Village],
+    scene: [MainScreen, Village, Game],
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {
-                y: 0
-            },
             debug: true
         }
-    }
+    },
 })
+
---
+++ awesome-game/src/scenes/Game.js
@@ -0,0 +1,69 @@
+import Phaser from '../lib/phaser.js'
+var cursors;
+var player;
+
+
+export default class MainScreen extends Phaser.Scene
+{
+    constructor()
+    {
+        super('game')
+        
+    }
+
+    preload()
+    {
+        this.load.image('background', 'assets/background_mainscreen.jpg');
+        this.load.image('hero', 'assets/ghost3.png');
+
+    }
+
+    create()
+    {
+
+
+        // add a background image
+        this.add.image(480, 320, 'background')
+        .setScrollFactor(1, 0)    
+        
+        
+        
+        player = this.physics.add.image(400, 300, 'hero');
+
+        cursors = this.input.keyboard.createCursorKeys();
+
+
+    }
+
+    update()
+    {
+
+        player.setVelocity(0);
+
+        if (cursors.left.isDown)
+        {
+            player.setVelocityX(-300);
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(300);
+        }
+
+        if (cursors.up.isDown)
+        {
+            player.setVelocityY(-300);
+        }
+        else if (cursors.down.isDown)
+        {
+            player.setVelocityY(300);
+        }
+
+        
+    }
+
+
+
+
+
+
+}
