ac7102394285cf44a4e4cbda806cc5c6cef662d3
Guenole76 <Gunol>
2021-03-18 15:30:32 +0100

    Boucle jeu

--- awesome-game/src/main.js
+++ awesome-game/src/main.js
@@ -3,13 +3,15 @@ import Phaser from './lib/phaser.js'
 import Village from './scenes/Village.js'
 import Forge from './scenes/Forge.js'
 import MainScreen from './scenes/MainScreen.js'
+import Bibli from './scenes/Bibliotheque.js'
+import Combat from './scenes/Castagne.js'
 
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 1340,
     height: 640,
-    scene: [MainScreen, Village, Forge],
+    scene: [MainScreen, Village, Forge, Bibli, Combat],
     physics: {
         default: 'arcade',
         arcade: {
