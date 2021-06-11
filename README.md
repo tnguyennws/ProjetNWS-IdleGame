50c9d8ec2e3216eaf21270addee39c4c5d47f6a5
Timothée Nguyen <tnguyen@normandiewebschool.fr>
2021-06-11 10:10:09 +0200

    Ajout level up + update README

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # ProjetNWS-IdleGame
 
-#### IdleGame est un jeu vidéo de type "cliquer"
+### IdleGame est un __jeu vidéo__ de type __"cliquer"__
 
-#### Le joueur incarne un personnage qui va devoir parcourir plusieur niveaux pour combattres des méchants pour gagner de l'or et de l'xp pour devenir plus fort.
+#### Le joueur incarne un personnage qui va devoir parcourir plusieurs niveaux pour combattres des méchants pour gagner de __l'or__ et de __l'xp__ pour devenir plus fort.
 
-#### Avec L'or gagner le joueur va pouvoir acheter des amélioration pour personnages pour augmenter ces stats comme ces PV(point de vie ), sont ATK(attaque) et sa DEF(défense).
+#### Avec L'or gagner le joueur va pouvoir acheter des amélioration pour son personnage pour augmenter ces stats comme ces PV(point de vie ), sont ATK(attaque) et sa DEF(défense).
+
+#### Le jeu comporte __4 mondes__ , chacun des mondes est composé de __4 niveaux__. Plus vous avancez dans les mondes, plus la difficulté augmente ! Au dernier niveau de chaque monde il y a un boss a battre après avoir combattu les méchants du niveau.
+
+#### Chaque Monstre vaincu rapporte :
+* 1 XP
+* 1GOLD
+
+
+##### Ce jeu a était crée par __Timothée Nguyen__ et __Guénolé LE GRAND__.
--- awesome-game/src/scenes/Forge.js
+++ awesome-game/src/scenes/Forge.js
@@ -5,10 +5,46 @@ export default class MainScreen extends Phaser.Scene {
     super("forge");
   }
 
+  init() {
+    if (localStorage.getItem("exp") > 0) {
+      this.vie = localStorage.getItem("vie");
+      this.atk = localStorage.getItem("atk");
+      this.def = localStorage.getItem("def");
+      this.niveau = localStorage.getItem("niveau");
+      this.or = localStorage.getItem("or");
+      this.exp = localStorage.getItem("exp");
+      this.arme = localStorage.getItem("arme");
+      this.armure = localStorage.getItem("armure");
+      this.win = localStorage.getItem("win");
+    } else {
+      this.vie = 100;
+      this.atk = 10;
+      this.def = 3;
+      this.niveau = 1;
+      this.or = 0;
+      this.exp = 0;
+      this.arme = 0;
+      this.armure = 0;
+      this.win = 0;
+
+      localStorage.setItem("vie", this.vie);
+      localStorage.setItem("atk", this.atk);
+      localStorage.setItem("def", this.def);
+      localStorage.setItem("niveau", this.niveau);
+      localStorage.setItem("or", this.or);
+      localStorage.setItem("exp", this.exp);
+      localStorage.setItem("arme", this.arme);
+      localStorage.setItem("armure", this.armure);
+      localStorage.setItem("win", this.win);
+    }
+  }
+
   preload() {
     this.load.image("forge", "assets/forge.jpg");
     this.load.image("player", "assets/hero-down-tp.png");
     this.load.image("acheter_niveau", "assets/titles/Acheter_un_niveau.png");
+    this.load.image("ameliorer_arme", "assets/titles/Ameliorer_larme.png");
+    this.load.image("ameliorer_armure", "assets/titles/Ameliorer_larmure.png");
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
@@ -16,16 +52,57 @@ export default class MainScreen extends Phaser.Scene {
   create() {
     this.add.image(500, 200, "forge").setScrollFactor(1, 0);
 
-    this.add.image()
+    var niveau = this.add.image(250, 200, "acheter_niveau").setScale(0.3);
+    var arme = this.add.image(650, 200, "ameliorer_arme").setScale(0.3);
+    var armure = this.add.image(1050, 200, "ameliorer_armure").setScale(0.3);
 
-    this.player = this.physics.add.sprite(240, 320, "player").setScale(1.5);
+    niveau.setInteractive();
+    arme.setInteractive();
+    armure.setInteractive();
+
+    niveau.on(
+      "pointerdown",
+      function() {
+        if(this.exp > this.niveau*150){
+            this.niveau++;
+            this.vie +=10;
+            this.atk += 2;
+            this.def++;
+            localStorage.setItem('niveau', this.niveau);
+            localStorage.setItem('vie', this.vie);
+            localStorage.setItem('atk', this.atk);
+            localStorage.setItem('def', this.def);
+        }else{
+            window.confirm("Vous n'avez pas assez d'expérience")
+        }
+      },
+      this
+    );
+
+    arme.on(
+      "pointerdown",
+      function() {
+        this.scene.start("forge");
+      },
+      this
+    );
+
+    armure.on(
+      "pointerdown",
+      function() {
+        this.scene.start("forge");
+      },
+      this
+    );
+
+    this.player = this.physics.add.sprite(650, 300, "player").setScale(1.5);
     this.player.setCollideWorldBounds(true);
 
     // set the horizontal dead zone to 1.5x game width
     this.cameras.main.setDeadzone(this.scale.width * 1);
 
     this.input.keyboard.once("keydown-SPACE", () => {
-      this.scene.start("victoire");
+      this.scene.start("village");
     });
   }
 
